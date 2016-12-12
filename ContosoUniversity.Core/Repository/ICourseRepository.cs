e8121a6bcd884d0fce95dff4827b7c7e46780c7d
mrazzano <mrazzano@comcast.net>
2016-12-12 15:52:16 -0800

    Updated README.md

---
+++ ContosoUniversity.Core/Repository/ICourseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using ContosoUniversity.Core.Models;
+
+namespace ContosoUniversity.Core.Repository
+{
+    public interface ICourseRepository
+    {
+        Task<IEnumerable<Course>> GetAsync();
+        Task<Course> GetAsync(int id);
+        Task<IEnumerable<Course>> GetByDepartmentIdAsync(int id);
+        void Add(Course entity);
+        void Update(Course entity);
+        void Delete(int id);
+    }
+}
